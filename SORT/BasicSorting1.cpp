5a7e36846c22866cb7bcc5f4372afb2743398b97
hjee02018@naver.com <hjee02018@naver.com>
2023-07-07 16:31:49 +0900

    [FIX] : 시간 초과 수정

--- SORT/BasicSorting1.cpp
+++ SORT/BasicSorting1.cpp
@@ -3,15 +3,39 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
-
+#include <map>
 using namespace std;
 
-vector<string> solution(vector<string> players, vector<string> callings)
+/*
+player: 현재
+callings : 해설
+answer : 최종
+*/
+
+
+// 시간 초과 개선을 위한 수정 사항 : map
+vector<string> solution1(vector<string> players, vector<string> callings)
 {
     vector<string> answer;
-    // player : 현재
-    // callings :해설
-    // answer : 최종
+    map<string, int> playerMap;
+    for (int i=0;i < players.size();i++)
+        playerMap[players[i]] = i;
+    for (int i = 0;i < callings.size();i++)
+    {
+        int idx = playerMap[callings[i]];
+        playerMap[players[idx]] = idx - 1;
+        playerMap[players[idx - 1]] = idx;
+
+        swap(players[idx], players[idx - 1]);
+    }
+
+    answer = players;
+    return answer;
+}
+
+vector<string> solution2(vector<string> players, vector<string> callings)
+{
+    vector<string> answer;
 
     for (int i = 0;i < callings.size();i++)
     {
