8ec1bb029bcc9f23093eca6b78913a63faecacb5
hjee02018@naver.com <hjee02018@naver.com>
2023-07-21 15:13:36 +0900

    [FIX]: 알고리즘 수정

--- math/findUsedCount.cpp
+++ math/findUsedCount.cpp
@@ -1,70 +1,61 @@
 #include <string>
 #include <vector>
+#include <algorithm>
+#include <iostream>
 
 using namespace std;
+
+int getVal(string mineral)
+{
+    if (mineral == "diamond")
+        return 3;
+    else if (mineral == "iron")
+        return 2;
+    else
+        return 1;
+}
 
 int solution(vector<int> picks, vector<string> minerals) 
 {
     int answer = 0;
+    int dia = picks[0];
+    int iron = picks[1];
+    int stone = picks[2];
     
-    int dia, iron, stone;
-    dia = picks[0];
-    iron = picks[1];
-    stone= picks[2];
+    // tot : ÃÑ °î±ªÀÌÀÇ ¼ö, tot_mineral : ÃÑ Ä¶ ¼ö ÀÖ´Â ±¤¹°ÀÇ ¼ö 
+    int tot = dia + iron + stone;
+    // Ä¶¼ö ÀÖ´Â°Å
+    int tot_mineral = tot * 5;
+    // Ä¶ °Å
+    int tot_minerals = minerals.size();
 
-    int idx = 0;
+    int real = min(tot_mineral, tot_minerals);
 
-    while (dia-- && idx < minerals.size())
+    vector <pair<int,int>> sum_of_val;
+    for (int i = 0;i < real;i+=5)
     {
-        for (int i = 0;i < 5;i++)
+        int sum = 0;
+        int j;
+        for (j = 0;j < 5;j++)
         {
-            if (idx >= minerals.size())
+            if (i+j >= real)
                 break;
-            answer++;
-            idx++;
+            sum += getVal(minerals[i + j]);
         }
-    }
-    
-    while (iron-- && idx < minerals.size())
-    {
-        for (int i = 0;i < 5;i++)
-        {
-            if (idx >= minerals.size())
-                break;
-            string mineral = minerals[idx];
-            if (mineral == "diamond")
-            {
-                answer += 5;
-            }
-            else
-                answer++;
-
-            idx++;
-        }
+        sum_of_val.push_back({i,sum });
     }
 
-    while (stone-- && idx < minerals.size())
-    {
-        for (int i = 0;i < 5;i++)
-        {
-            if (idx >= minerals.size())
-                break;
-            string mineral = minerals[idx];
-            if (mineral == "diamond")
-                answer += 25;
-            else if (mineral == "iron")
-                answer += 5;
-            else
-                answer++;
-            idx++;
-        }
-    }
+    sort(sum_of_val.begin(), sum_of_val.end(),greater<>());
 
+
+   
     return answer;
 }
 
+
+
 int main()
 {
-    solution({ 1,3,2 },{ "diamond", "diamond", "diamond", "iron", "iron", "diamond", "iron", "stone" });
+    solution({ 1,1,0 },{ "stone", "stone", "iron", "stone", "diamond", "diamond", "diamond", "diamond", "diamond", "diamond" });
     return 0;
 }
